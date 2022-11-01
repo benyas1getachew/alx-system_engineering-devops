e85fb0787cab795d200ad2151327e30e17bf47f7
benyas1getachew <99409114+benyas1getachew@users.noreply.github.com>
2022-11-01 23:14:32 +0530

    my commit

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+parse titles of all hot articles and
+prints a sorted count of given keywords
+"""
+import json
+import re
+import requests
+import sys
+
+headers = {'User-Agent': 'My User Agent 1.0'}
+
+
+def count_words(subreddit, word_list):
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces) """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    storage = {word.lower(): 0 for word in word_list}
+    r = requests.get(url, headers=headers)
+    if (r.status_code == 404 or 'data' not in r.json()):
+        return None
+    else:
+        while (1):
+            r = r.json()
+            for post in r['data']['children']:
+                tmp = post['data']['title'].split()
+                for word in tmp:
+                    if word.lower() in storage.keys():
+                        storage[word.lower()] += 1
+            after = r['data']['after']
+            if (after is None):
+                break
+            r = requests.get("{}?after={}".format(url, after), headers=headers)
+    storage = [(k, storage[k]) for k in
+               sorted(storage, key=storage.get, reverse=True)]
+    if len(storage) == 0:
+        print("")
+    else:
+        for k, v in storage:
+            if (v > 0):
+                print("{}: {:d}".format(k, v))
