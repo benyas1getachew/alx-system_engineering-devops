3dcc4ea6d7e88a4d8bae995aa42a44cb1340a39e
benyas1getachew <99409114+benyas1getachew@users.noreply.github.com>
2022-11-02 00:12:21 +0530

    my commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
 """
-script that queries Reddit API and returns
-number of subscribers for a given subreddit.
-if invalid, return 0
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
 """
-import json
 import requests
 import sys
 
-headers = {'User-Agent': 'My User Agent 1.0'}
-
 
 def number_of_subscribers(subreddit):
-	"""
-	function that returns the number
-	of subscribers
-	"""
-	try:
-		url = 'https://www.reddit.com/r/'
-		response = requests.get(url + subreddit + "/about.json",
-					headers=headers, allow_redirects=False)
-		return response.json()['data']['subscribers']
-	except:
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
+        return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
 		return 0
